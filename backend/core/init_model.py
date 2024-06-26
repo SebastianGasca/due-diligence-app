f2d599444308f403b54a6d6764e66252f025d660
SebastianGasca <sebagasca@gmail.com>
2024-06-26 09:43:08 -0400

    changes in init_model, added remote conection to bert model

--- backend/core/init_model.py
+++ backend/core/init_model.py
@@ -6,7 +6,15 @@ import torch
 
 def load_model(model_path = "models/bert2bert_shared-spanish-finetuned-summarization"):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    state_dict = load_file(f'{model_path}/model.safetensors')
-    model = EncoderDecoderModel.from_pretrained(model_path, state_dict=state_dict).to(device)
-    tokenizer = BertTokenizerFast.from_pretrained(model_path)
+    
+    # LOCAL
+    # state_dict = load_file(f'{model_path}/model.safetensors')
+    # model = EncoderDecoderModel.from_pretrained(model_path, state_dict=state_dict).to(device)
+    # tokenizer = BertTokenizerFast.from_pretrained(model_path)
+    
+    # INTERNET
+    ckpt = 'mrm8488/bert2bert_shared-spanish-finetuned-summarization'
+    model = EncoderDecoderModel.from_pretrained(ckpt).to(device)
+    tokenizer = BertTokenizerFast.from_pretrained(ckpt)
+    
     return model, tokenizer
